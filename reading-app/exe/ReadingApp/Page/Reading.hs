c18e45eb3079efbe186fe5ae7bf157d4ce8f4cdf
Tom Cumming <mail@tomcumming.co.uk>
2024-10-29 22:25:13 +0000

    Form post

--- reading-app/exe/ReadingApp/Page/Reading.hs
+++ reading-app/exe/ReadingApp/Page/Reading.hs
@@ -12,12 +12,26 @@ import Servant.HTML.Blaze qualified as B (HTML)
 import Servant.Server qualified as Sv
 import Text.Blaze.Html qualified as B
 import Text.Blaze.Html5 qualified as B
+import Text.Blaze.Html5.Attributes qualified as Attr
+import Web.FormUrlEncoded qualified as Form
+
+newtype NextPhrasePost = NextPhrasePost T.Text
+
+instance Form.FromForm NextPhrasePost where
+  fromForm fm =
+    NextPhrasePost
+      <$> Form.parseUnique "phrase" fm
 
 data Routes mode = Routes
   { rtNextPhrase ::
       mode
         Sv.:- "next-phrase"
           Sv.:> Sv.Get '[B.HTML] B.Html,
+    rtNextPhrasePost ::
+      mode
+        Sv.:- "next-phrase"
+          Sv.:> Sv.ReqBody '[Sv.FormUrlEncoded] NextPhrasePost
+          Sv.:> Sv.Post '[B.HTML] B.Html,
     rtRoot :: mode Sv.:- Sv.Get '[Sv.PlainText] Sv.NoContent
   }
   deriving (Generic)
@@ -30,7 +44,15 @@ server readThroughId =
     { rtNextPhrase = do
         pure $ wrapper $ do
           B.h1 "Enter phrase characters"
-          B.p (B.string (show readThroughId)),
+          B.form
+            B.! Attr.method "post"
+            $ do
+              B.input
+                B.! Attr.name "phrase"
+                B.! Attr.type_ "text"
+              B.input B.! Attr.type_ "submit",
+      rtNextPhrasePost = \(NextPhrasePost phrase) -> do
+        pure $ wrapper $ B.h1 (B.text phrase),
       rtRoot = do
         -- TODO safe link
         let url =
